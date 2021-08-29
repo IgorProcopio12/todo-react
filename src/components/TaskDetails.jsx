25295f81ebd3b11a90a98e6985de5e4aa3ad4233
Igor Procópio <procopio.igor@hotmail.com>
2021-08-29 18:21:01 -0300

    textField

--- src/components/TaskDetails.jsx
+++ src/components/TaskDetails.jsx
@@ -1,31 +1,33 @@
-import React from 'react';
+import React from "react";
+import { TextField } from "@material-ui/core";
 
-
-import { CgChevronLeft} from 'react-icons/cg';
-import { useHistory, useParams } from 'react-router-dom';
-import Button from './Button';
+import { CgChevronLeft } from "react-icons/cg";
+import { useHistory, useParams } from "react-router-dom";
+import Button from "./Button";
 import "./TaskDetails.css";
 
 const TaskDetails = () => {
-    const params = useParams();
-    const history = useHistory();
+  const params = useParams();
+  const history = useHistory();
 
-    const handleBackButtonClick = () => {
-        history.goBack();
-    }
-    return (
-        <>
-            <div className="back-button-container"></div>
-                <button className="back-button" onClick={handleBackButtonClick}>
-                    <CgChevronLeft />
-                </button>
-            
-            <div className="task-details-container">
-                <h2>{params.taskTitle}</h2>
-                <p>descrição</p>
-            </div>
-        </>
-    );
-}
+  const handleBackButtonClick = () => {
+    history.goBack();
+  };
+  return (
+    <>
+      <div className="back-button-container"></div>
+      <button className="back-button" onClick={handleBackButtonClick}>
+        <CgChevronLeft />
+      </button>
+
+      <div className="task-details-container">
+        <h2>{params.taskTitle}</h2>
+        <form>
+          <TextField className="form-container" id="outlined-basic" label="Descrição" variant="outlined" />
+        </form>
+      </div>
+    </>
+  );
+};
 
 export default TaskDetails;
